b9cbc0c119da06f13cdbb52cce81cc0fb3f938ce
HyderJd12 <148965113+HyderJd12@users.noreply.github.com>
2023-11-07 21:44:06 +0500

    w3c2 (JS class assignment)

---
+++ w2task/class assignment.js
@@ -0,0 +1,18 @@
+// Number of array in Ascending order :
+console.log("ASC")
+const acs = [1,2,15,4,11,8,6,4];
+const sortedArray = acs.sort( (a,b) => a - b );
+console.log (sortedArray);
+
+// Number of array in Descending order :
+const num=[2,4,1,5,6,3];
+console.log("DESC")
+const unsorted = num.sort( (a,b) => b - a );
+console.log (unsorted);
+
+// String shorting ascending 
+console.log("");
+console.log("-------string-----------");
+const names = ['maham', 'farman', 'Mj', 'duli' ,'Ali', 'ali'];
+const namearray = names.sort ((a,b) => a.localeCompare (b) );
+console.log(namearray);
